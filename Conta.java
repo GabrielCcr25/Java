4ccd6d8ef0d4d7ecdd0fcefbe4aec7343968cf1e
Gabriel Lima <108353430+GabrielCcr25@users.noreply.github.com>
2022-09-15 21:35:42 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,29 @@
+package br.com.newton.banco;
+
+public class Conta {
+	private String numero;
+	private double saldo;
+	public Conta(String numero, double saldo) {
+		super();
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo=this.saldo+valor;
+	}
+	
+
+}
